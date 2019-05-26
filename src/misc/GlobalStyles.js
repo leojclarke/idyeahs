86e091dacf7a7294104bd80bbcbbd8bf1d87b0c9
Leo Clarke <leojclarke@gmail.com>
2019-05-26 21:34:15 +0200

    Add favicon and general styling

--- src/misc/GlobalStyles.js
+++ src/misc/GlobalStyles.js
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+
+/* RGB */
+$color1: rgba(127, 183, 190, 1);
+$color2: rgba(211, 243, 238, 1);
+$color3: rgba(218, 204, 62, 1);
+$color4: rgba(188, 44, 26, 1);
+$color5: rgba(125, 21, 56, 1);
+
   * {
     box-sizing: border-box;
   }
@@ -9,7 +17,7 @@ export default createGlobalStyle`
     margin: 0;
     padding: 20px;
     font-family: sans-serif;
-    color: darkslategray;
-    background: papayawhip;
+    color: white;
+    background: rgba(127, 183, 190, 1);
   }
 `
