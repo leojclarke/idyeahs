1dea724f1d14633e5147b6c6736f5c133e153b21
Leo Clarke <leojclarke@gmail.com>
2019-05-27 23:20:36 +0200

    Begin Routing

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from '@reach/router';
 
 const StyledFooter = styled.footer`
   background: rgba(127, 183, 190, 1);
   color: white;
-`
+`;
 
 export default function Footer() {
-  return <StyledFooter />
+  return (
+    <StyledFooter>
+      <Link to="/">IDEAS</Link>
+      <Link to="/add-idea">ADD IDEA</Link>
+    </StyledFooter>
+  );
 }
