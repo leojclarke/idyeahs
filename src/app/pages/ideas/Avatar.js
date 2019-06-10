79e29399e8262f4f8ad093b674f2c438c6ffb7dc
Leo Clarke <leojclarke@gmail.com>
2019-06-10 16:07:16 +0200

    Work on card details

---
+++ src/app/pages/ideas/Avatar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainAvatar = styled.img`
+  clip-path: circle(30px at center);
+  width: 60px;
+  margin: 10px;
+  border: 1.5px solid #6558f5;
+  border-radius: 50%;
+`;
+
+const MediumAvatar = styled.img`
+  clip-path: circle(25px at center);
+  width: 50px;
+  margin: 10px;
+  border: 1.5px solid #6558f5;
+  border-radius: 50%;
+`;
+
+const SmallAvatar = styled.img`
+  clip-path: circle(15px at center);
+  width: 30px;
+  margin: 10px;
+  border: 1px solid #6558f5;
+  border-radius: 50%;
+`;
+
+export function Avatar({ src }) {
+  return <MainAvatar src={src} />;
+}
+
+export function CardAvatar({ src }) {
+  return <MediumAvatar src={src} />;
+}
+
+export function CommentAvatar({ src }) {
+  return <SmallAvatar src={src} />;
+}
